ffa49b45356b7cf4677de756eb2f8fe7bb10784e
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-09-06 00:05:29 +0530

    [Rushikesh] Bubble sort

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -5,7 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            InsertionSort insertionSort = new InsertionSort("English","Marathi","Hindi","Telugu","Punjabi");
+            
+            Console.WriteLine("Algorithm Programs");
+            Console.WriteLine("Press 1 for Insertion Sort");
+            Console.WriteLine("Press 2 for Bubble Sort");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    InsertionSort insertionSort = new InsertionSort("English", "Marathi", "Hindi", "Telugu", "Punjabi");
+                    break;
+                case 2:
+                    BubbleSort bubbleSort = new BubbleSort("English", "Marathi", "Hindi", "Telugu", "Punjabi");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    break;
+            }
 
         }
     }
