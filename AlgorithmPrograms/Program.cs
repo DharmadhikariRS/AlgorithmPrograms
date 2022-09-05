52f4534caf75b3a686f7ec2104b6e13311895b86
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-09-05 23:40:17 +0530

    [Rushikesh] insertion sort

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            InsertionSort insertionSort = new InsertionSort("English","Marathi","Hindi","Telugu","Punjabi");
+
+        }
+    }
+}
